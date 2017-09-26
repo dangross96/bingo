821c021f8cea5240c06fbd74a4bab82b9cee331a
Daniel Gross <dangross@wirelessprv-10-193-89-99.near.illinois.edu>
2017-09-26 18:18:52 -0500

    Bingo board generation code

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// nain.cpp for board generation
+
+#include <cstdlib>
+#include <stdio.h>
+#include <iostream>
+
+#include "BoardGenerator.h"
+
+int main( int argc, char* argv[] )
+{
+	BoardGenerator::generate_board();
+
+	return 0;
+}
